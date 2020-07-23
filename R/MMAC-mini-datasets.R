d2c86368be293f0fe5c2d6ad5cac774f88d0c6e4
Danny Kaplan <kaplan@macalester.edu>
2020-07-23 16:28:34 -0600

    cleaning

---
+++ R/MMAC-mini-datasets.R
@@ -0,0 +1,3 @@
+Flying_speed <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSHa2cDtYLxJkxccQvU_8KQySz7GsT7zaOBP01UBMm2OGDuqDhtFA00GnIhI81C5e5kRQ2RTkV5KgX4/pub?gid=0&single=true&output=csv"
+
+
